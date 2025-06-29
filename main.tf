15e0fecbef2a1178d3584f20fb3ffcfcbece1cbd
megha <meghaviveki@gmail.com>
2025-06-29 16:06:48 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "meghavm" {
+  ami           = "ami-0731becbf832f281e"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-07af1672b9c751ce8"
+  key_name = "megha-key"
+
+  tags = {
+    Name = "meghavm"
+  }
+}
+resource "aws_eip" "lb" {
+  instance = aws_instance.meghavm.id
+  domain   = "vpc"
+}
+output "public_IP" {
+  value = aws_eip.lb.public_ip
+}
